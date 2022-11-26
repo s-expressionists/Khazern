bbe522db36a350badfac5aeff4d08f144a458317
Tarn W. Burton <twburton@gmail.com>
2022-11-26 15:50:59 -0500

    Use without-package-locks

--- code/intrinsic.lisp
+++ code/intrinsic.lisp
@@ -1,5 +1,5 @@
 (in-package :cl-user)
 
-(trivial-package-locks:with-unlocked-packages (:cl)
+(trivial-package-locks:without-package-locks
   (setf (macro-function 'loop) (macro-function 'khazern:loop)
         (macro-function 'loop-finish) (macro-function 'khazern:loop-finish)))
