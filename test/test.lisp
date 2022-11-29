3ab7292d6ee8891ef72d0ce07642d6b6cc8a1d6c
Tarn W. Burton <twburton@gmail.com>
2022-11-29 13:12:36 -0500

    Put test forms second

--- test/test.lisp
+++ test/test.lisp
@@ -2,39 +2,47 @@
 
 (define-test loop-finish-in-simple-loop
   (is equal
-      (khazern:loop do (khazern:loop (khazern:loop-finish)))
-      nil))
+      nil
+      (khazern:loop do (khazern:loop (khazern:loop-finish)))))
 
 (define-test loop-until-t
   (is equal
-      (khazern:loop until t)
-      nil))
+      nil
+      (khazern:loop until t)))
 
 (define-test loop-while-nil
   (is equal
-      (khazern:loop while nil)
-      nil))
+      nil
+      (khazern:loop while nil)))
 
 (define-test loop-until-expr
   (is equal
+      10
       (let ((n 0))
         (khazern:loop until (= (incf n) 10))
-        n)
-      10))
+        n)))
 
 (define-test loop-while-expr
   (is equal
+      10
       (let ((n 0))
         (khazern:loop while (< (incf n) 10))
-        n)
-      10))
+        n)))
 
 (define-test loop-do
   (is equal
+      10
       (let ((n 0))
         (block abc
           (khazern:loop do (progn (incf n)
                              (when (= n 10)
                                (return-from abc nil)))))
-        n)
-      10))
+        n)))
+
+#+(or)(define-test loop-and-init
+  (is equal
+      '(-1 0 2 5 9)
+      (let ((i -1))
+        (loop for i from 1 to 5 and
+                  j = i then (+ j i)
+              collect j))))
