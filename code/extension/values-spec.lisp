88ce35541a423352e33101ec8c525521a2abe506
Tarn W. Burton <twburton@gmail.com>
2025-12-03 10:37:08 -0500

    Some cleanup

--- code/extension/values-spec.lisp
+++ code/extension/values-spec.lisp
@@ -1,12 +1,27 @@
 (cl:in-package #:khazern-extension)
 
+(defun values-specifier-p (object)
+  (and (consp object)
+       (eq (car object) 'values)))
+
+(deftype values-specifier ()
+  `(satisfies values-specifier-p))
+
 (defmethod khazern:parse-var-spec ((client extension-client) &key (type-spec t))
   (multiple-value-bind (foundp specifier)
       (khazern:maybe-parse-token :type 'values-specifier)
     (if foundp
-        (khazern::make-values-binding (khazern:with-tokens (cdr specifier)
-                                        (loop while (khazern:more-tokens-p)
-                                              collect (khazern:parse-var-spec client :type-spec type-spec)))
-                                      :type type-spec
-                                      :ignorable t)
+        (khazern:with-tokens (cdr specifier)
+          (prog (vars)
+           next
+             (when (khazern:more-tokens-p)
+               (push (khazern:parse-var-spec client
+                                             :type-spec type-spec)
+                     vars)
+               (go next))
+             (return (if (cdr vars)
+                         (khazern::make-values-binding (nreverse vars)
+                                                       :type type-spec
+                                                       :ignorable t)
+                         (car vars)))))
         (call-next-method))))
